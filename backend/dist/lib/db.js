e14976b5d8d64610a1cfeed865eb3fbcba9abe8e
abhishekkkk-15 <mrabhi748@gmail.com>
2025-03-13 14:40:23 +0530

    deployment 9

--- backend/dist/lib/db.js
+++ backend/dist/lib/db.js
@@ -2,4 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.db = void 0;
 const client_1 = require("@prisma/client");
-exports.db = new client_1.PrismaClient();
+const globalForPrisma = global;
+exports.db = globalForPrisma.prisma ||
+    new client_1.PrismaClient({
+        datasources: {
+            db: {
+                url: process.env.DATABASE_URL + "?connection_limit=5", // Limit connections
+            },
+        },
+    });
+if (process.env.NODE_ENV !== "production") {
+    globalForPrisma.prisma = exports.db;
+}
